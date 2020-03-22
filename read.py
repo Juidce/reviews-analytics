81236609d33d447c627eef7fc0140431756345b0
Juidce <lyn49ha@gmail.com>
2020-03-22 16:15:01 +0800

    add filtering

--- read.py
+++ read.py
@@ -8,10 +8,15 @@ with open('reviews.txt', 'r') as f:
 			print(len(data))
 print('檔案讀取完畢，總共有', len(data),'筆資料。')
 
-sum_len = 0  #為何要先設定為0?
+sum_len = 0
 for d in data:
 	sum_len = sum_len + len(d)
 
-print('每筆留言平均長度為', sum_len/len(data),'個字')
-# 剩下留言長度加總以及平均長度的計算。
-# len是長度，len(d)是d的長度，d是data內的每一筆資料。len(d)為每一筆資料的長度。
+print('每筆留言平均長度為', sum_len/len(data), '個字')
+
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print('一共有', len(new), '筆留言長度小於100個字')
+print(new[0])
